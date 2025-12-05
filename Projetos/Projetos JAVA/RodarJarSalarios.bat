99d95e6e31fc77579661db50f06ae98cf8cd21cc
ygor202525 <engeletricygor@gmail.com>
2025-12-05 09:52:48 -0300

    Delete Projetos/Projetos JAVA/RodarJarSalarios.bat

--- Projetos/Projetos JAVA/RodarJarSalarios.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set JAVA_EXE="C:\Program Files\Java\jdk-17\bin\java.exe"
-set JAR_FILE="C:\Users\alcan\OneDrive\Documentos\PROJETOS IDE ECLIPSE\Novamente.jar"
-
-echo Iniciando programa...
-%JAVA_EXE% -jar %JAR_FILE%
-pause
